221e0795c7e1698d936354f25937d3bb2f4fd5ed
Jeremy Greer <jex.grizzle@gmail.com>
2022-05-07 14:45:46 -0400

    Add incrementing turn

--- basic_game/__main__.py
+++ basic_game/__main__.py
@@ -1,11 +1,24 @@
+from copy import deepcopy
+from time import sleep
+
+
 class State:
     game_over: bool
+    turn: int
 
-    def __init__(self) -> None:
+    def __init__(self, **kwargs) -> None:
         self.game_over = False
+        self.turn = 0
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
     def __str__(self) -> str:
-        return f"game_over: {self.game_over}"
+        fields = [
+            "game_over",
+            "turn",
+        ]
+        lines = [f"{field}: {getattr(self, field)}" for field in fields]
+        return "\n".join(lines)
 
 
 class Input:
@@ -13,7 +26,9 @@ class Input:
 
 
 def update_state(state: State, input: Input) -> State:
-    return State()
+    next_state = deepcopy(state)
+    next_state.turn = next_state.turn + 1
+    return next_state
 
 
 def present(state: State) -> None:
@@ -22,23 +37,34 @@ def present(state: State) -> None:
 
 
 def get_input() -> Input:
+    sleep(1)
     return Input()
 
 
+saved_state = State()
+
+
 def load_state() -> State:
-    return State()
+    return saved_state
 
 
 def save_state(state: State) -> None:
+    global saved_state
+    saved_state = state
     return None
 
 
 def game_loop() -> None:
     state = load_state()
-    input = get_input()
-    next_state = update_state(state, input)
-    save_state(next_state)
-    present(next_state)
+    while True:
+        state = load_state()
+        present(state)
+        input = get_input()
+        next_state = update_state(state, input)
+        save_state(next_state)
 
 
 game_loop()
+
+
+    
